d2683854cb8390c0e1c3844a35b1c906c9671c23
MarceauKa <marceau@casals.fr>
2016-03-01 22:15:31 +0100

    Add the French localization

---
+++ resources/lang/fr/addon.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'       => 'Recherche',
+    'name'        => 'Module Recherche',
+    'description' => 'Un puissant module de recherche pour Streams Platform.',
+    'section'     => [
+        'index' => 'Index de recherche'
+    ]
+];
---
+++ resources/lang/fr/permission.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'index' => [
+        'name'  => 'Index de recherche',
+        'option' => [
+            'read'    => 'Peut accèder à l\'index de recherche ?',
+            'delete'  => 'Peut supprimer les entrées ?',
+            'rebuild' => 'Peut reconstruire l\'index de recherche ?'
+        ]
+    ]
+];
