222334106acd86524e8c38ad8fafc9f7ae011c5f
Jack Sun <sunhsilu@gmail.com>
2016-07-01 13:59:18 +0800

    Add Chinese Traditional localization

---
+++ resources/lang/zh-tw/button.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'search' => '搜尋',
+    'rebuild' => '重建索引'
+];
---
+++ resources/lang/zh-tw/field.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'term' => [
+        'placeholder' => '搜尋...'
+    ],
+    'title' => [
+        'name' => '標題'
+    ],
+    'description' => [
+        'name' => '敘述'
+    ],
+    'keywords' => [
+        'name' => '關鍵字'
+    ],
+    'stream' => [
+        'name' => '資料流'
+    ]
+];
