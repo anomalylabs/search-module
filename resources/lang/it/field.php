7807bfed1b26721b9ca4d4981a0066bac8ac8973
Christian Giupponi <chri788@gmail.com>
2016-07-20 17:09:42 +0200

    Add italian translation

---
+++ resources/lang/it/field.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'term' => [
+        'placeholder' => 'Cerca...',
+    ],
+    'title' => [
+        'name' => 'Titolo',
+    ],
+    'description' => [
+        'name' => 'Descrizione',
+    ],
+    'keywords' => [
+        'name' => 'Keywords',
+    ],
+    'stream' => [
+        'name' => 'Stream',
+    ],
+];
---
+++ resources/lang/it/permission.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'index' => [
+        'name' => 'Ricerca Index',
+    'option' => [
+        'read' => 'Può accedere alla ricerca?',
+    'delete' => 'Può cancellare indici?',
+    'rebuild' => 'Può ricreare gli indici di ricerca?',
+    ],
+    ],
+];
