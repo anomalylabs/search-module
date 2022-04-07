ba610fdb29896ad26edd55bc31e27dfeefca85d2
Metehan Celik <mthncelik@gmail.com>
2022-04-07 12:22:07 +0300

    admin panel translations

---
+++ resources/lang/tr/permission.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'items' => [
+        'name'   => 'Öğeler',
+        'option' => [
+            'read'   => 'Öğeyi okuyabilir mi?',
+            'write'  => 'Öğe oluşturabilir/düzenleyebilir mi?',
+            'delete' => 'Öğe silebilir mi?',
+        ],
+    ],
+];
